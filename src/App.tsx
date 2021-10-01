3a6c864cf5c355125274af5a11188fd41ce590e3
UJ528491 <hyj0829@gmail.com>
2021-10-01 23:46:03 +0900

    typing useState as props

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import React, {
 import "./App.css"
 
 const Heading = ({ title }: { title: string }) => <h2>{title}</h2>
-
 const Box: React.FunctionComponent = ({ children }) => (
   <div
     style={{
@@ -19,7 +18,6 @@ const Box: React.FunctionComponent = ({ children }) => (
     {children}
   </div>
 )
-
 const List: React.FunctionComponent<{
   items: string[]
   onClick?: (item: string) => void
@@ -33,18 +31,96 @@ const List: React.FunctionComponent<{
   </ul>
 )
 
+interface Payload {
+  text: string
+}
+interface Todo {
+  id: number
+  text: string
+}
+type ActionType =
+  | {
+      type: "ADD"
+      text: string
+    }
+  | {
+      type: "REMOVE"
+      id: number
+    }
+
+const Incrementer: React.FunctionComponent<{
+  value: number
+  setValue: React.Dispatch<React.SetStateAction<number>>
+}> = ({ value, setValue }) => (
+  <button onClick={() => setValue(value + 1)}>Add - {value}</button>
+)
+
 function App() {
   const onListClick = useCallback((item: string) => {
     alert(item)
   }, [])
-  const [checkedState, setCheckedState] = useState(true)
+
+  const [payload, setPayload] = useState<Payload | null>(null)
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then(resp => resp.json())
+      .then(data => {
+        setPayload(data)
+      })
+  })
+
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+            done: false,
+          },
+        ]
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id)
+      default:
+        throw new Error()
+    }
+  }, [])
+
+  const newTodoRef = useRef<HTMLInputElement>(null)
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      })
+    }
+  }, [])
+  const [value, setValue] = useState(0)
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello there</Box>
       <List items={["one", "two", "three"]} onClick={onListClick} />
+      <Box>{JSON.stringify(payload)}</Box>
+      <Incrementer value={value} setValue={setValue} />
+
+      <Heading title="Todos" />
+      {todos.map(todo => (
+        <div key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+            Remove
+          </button>
+        </div>
+      ))}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>onAdd</button>
+      </div>
     </div>
   )
 }
-
 export default App
